b443a751e096f2543674f6dcd155a9eddc0c9b33
Steffan Achtmann <Steffan Achtmann>
2016-03-09 16:05:44 -0800

    Fixed superlist->superlists typo

--- deploy_tools/fabfile.py
+++ deploy_tools/fabfile.py
@@ -36,13 +36,13 @@ def _get_latest_version(source_folder):
 
 
 def _update_settings(source_folder, site_name):
-    settings_path = source_folder + '/superlist/settings.py'
+    settings_path = source_folder + '/superlists/settings.py'
     sed(settings_path, 'DEBUG = True', 'DEBUG = False')
     sed(settings_path,
         'ALLOWED_HOSTS = .+$',
         'ALLOWED_HOSTS = ["%s"]' % (site_name,)
     )
-    secret_key_file = source_folder + "/superlist/secret_key.py"
+    secret_key_file = source_folder + "/superlists/secret_key.py"
     if not exists(secret_key_file):
         chars = 'abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)'
         key = ''.join(random.SystemRandom().choice(chars) for _ in range(50))
